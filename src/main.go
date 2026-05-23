d76890837875f6106754206f33679dc016a1644e
the.artificilis <nils.schatti@hesge.ch>
2026-05-23 15:03:11 +0200

    Test

--- src/main.go
+++ src/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cash <command>")
+	}
+
+	cmd := os.Args[1]
+
+	switch cmd {
+	case "add":
+		amount := os.Args[2]
+		currency := os.Args[3]
+		product := os.Args[4]
+		var shop string
+		if len(os.Args) > 5 {
+			shop = os.Args[5]
+		}
+		fmt.Println("Adding money")
+	default:
+		fmt.Println("Unknown command")
+	}
+}
